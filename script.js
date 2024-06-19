aeefacd2ff1ec38c8cb5bd80a69b0c4db192a6c3
PrikolBoy <Sergey46710a@gmail.com>
2024-06-19 14:38:42 +0700

    Init

--- script.js
+++ script.js
@@ -1,61 +1,102 @@
-const startButtonSelector = ".button__start";
-const stopButtonSelector = ".button__stop";
-const hoursButtonSelector = "#hours";
-const minutesInputSelector = "#minutes";
-const secondsInputSelector = "#seconds";
+const map = document.querySelector("canvas");
+const canvas = map.getContext('2d');
+canvas.fillStyle = "rgb(255, 255, 255)";
 
-const hoursInput = document.querySelector(hoursButtonSelector);
-const minutesInput = document.querySelector(minutesInputSelector);
-const secondsInput = document.querySelector(secondsInputSelector);
-const StartBtn = document.querySelector(startButtonSelector);
-const StopBtn = document.querySelector(stopButtonSelector);
+const grid = 15;
+const paddleHeight = grid * 5;
+const maxPaddlyY = map.height - grid - paddleHeight;
 
-let remainingTime;
+let ballSpeed = 5;
+let paddleSpeed = 7;
 
-let intervalID;
-let hours;
-let minutes;
-let seconds;
-
-setTimeout(() => {
-
-}, )
-
-function startTimer(event) {
-    event.preventDefault();
-    hours = parseInt(hoursInput.value);
-    minutes = parseInt(minutesInput.value);
-    seconds = parseInt(secondsInput.value);
-
-    remainingTime = hours * 3600 + minutes * 60 + seconds;
-    StartBtn.classList.add('hide');
-    StopBtn.classList.remove('hide');
-
-    intervalID = setInterval(updateTimer, 1000);
+const leftPaddle = {
+    x: grid * 2,
+    y: map.height / 2 - paddleHeight / 2,
+    width: grid,
+    height: paddleHeight,
+    dy: 0,
 }
 
-function updateTimer() {
-    remainingTime = remainingTime -1;
+const rightPaddle = {
+    x: map.width - grid * 3,
+    y: map.height / 2 - paddleHeight / 2,
+    width: grid,
+    height: paddleHeight,
+    dy: 0,
+}
 
-    hours = Math.floor(remainingTime / 3600);
-    minutes = Math.floor(remainingTime % 3600 / 60);
-    seconds = remainingTime % 60;
+const ball = {
+    x: map.width / 2,
+    y: map.height / 2,
+    width: grid,
+    height: grid,
+    dx: ballSpeed,
+    dy: -ballSpeed,
+    resetting: false,
+    isResetted: false,
+}
 
-    hoursInput.value = hours.toString().padStart(2, "0");
-    minutesInput.value = minutes.toString().padStart(2, "0");
-    secondsInput.value = seconds.toString().padStart(2, "0");
+function renderMap() {
+    canvas.fillRect(0, 0, map.width, grid); // Верхняя граница
+    canvas.fillRect(0, map.height - grid, map.width, grid) // Нижняя граница
 
-    if (remainingTime == 0) {
-        stopTimer();
+    for (let i = grid; i < map.height - grid; i += grid * 2) {
+        canvas.fillRect(map.width / 2, i, grid, grid); // Разделительная линия
     }
 }
 
-function stopTimer() {
-    clearInterval(intervalID);
-    StartBtn.classList.remove('hide');
-    StopBtn.classList.add('hide');
+function clearMap() {
+    canvas.clearRect(0, 0, map.width, map.height);
 }
 
+function renderLeftPaddle() {
+    canvas.fillRect(leftPaddle.x, leftPaddle.y, leftPaddle.width, leftPaddle.height);
+}
 
-StartBtn.addEventListener("click", startTimer);
-StopBtn.addEventListener("click", stopTimer);
+function renderRightPaddle() {
+    canvas.fillRect(rightPaddle.x, rightPaddle.y, rightPaddle.width, rightPaddle.height);
+}
+
+function movePaddles() {
+    leftPaddle.y += leftPaddle.dy;
+    rightPaddle.y += rightPaddle.dy;
+}
+
+function renderBall() {
+    canvas.fillRect(ball.x, ball.y, ball.width, ball.height);
+}
+
+function moveBall() {
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+function loop () {
+    clearMap();
+
+    renderLeftPaddle();
+    renderRightPaddle();
+    movePaddles();
+    
+    renderBall();
+    moveBall();
+
+    renderMap();
+    requestAnimationFrame(loop);
+}
+
+document.addEventListener('keydown', (event) => {
+    if (event.key === 'w' || event.key === 'ц') {
+        leftPaddle.dy = -paddleSpeed;
+    } else if (event.key === 's' || event.key === 'ы') {
+        leftPaddle.dy = paddleSpeed;
+    }
+})
+
+document.addEventListener('keyup', (event) => {
+    if (event.key === 'w' || event.key === 's' || event.key === 'ы' || event.key === 'ц') {
+        leftPaddle.dy = 0; 
+    }
+})
+
+requestAnimationFrame(loop);
