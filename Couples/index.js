181f2a68df7ffdfce036e06b1adf27644d57b68c
PrikolBoy <Sergey46710a@gmail.com>
2024-06-30 11:48:47 +0700

    upd

--- Couples/index.js
+++ Couples/index.js
@@ -1,10 +1,23 @@
 const board = document.querySelector(".board");
-const boardBTN = document.querySelector(".board__button")
+const boardBTN = document.querySelector('.board__button');
+const BoardInput = document.querySelector('.board__input');
 
-function createBoard(event) {
-    event.preventDefault();
+boardBTN.addEventListener('click', (event) => {
+    event.preventDefault()
+    
+    let colums = input.value;
+    let count;
 
+    if (colums >= 2 && colums <= 6 && colums % 2 == 0) {
+        count = colums * colums;
+    } else {
+        alert("Нужно написать четное число в указанном диапазоне.");
+        return;
+    }
+
+    createBoard(count, colums);
+})
+
+function createBoard(count, colums) {
     board.textContent = "";
 }
-
-boardBTN.addEventListener('click', createBoard())
