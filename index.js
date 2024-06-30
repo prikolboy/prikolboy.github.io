42494575e9f0fae8753cfeefd7ef8738b8c46130
PrikolBoy <Sergey46710a@gmail.com>
2024-06-30 14:48:54 +0700

    fixed

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-const board = document.querySelector(".board");
+const board = document.querySelector('.board');
 const boardBTN = document.querySelector('.board__button');
 const BoardInput = document.querySelector('.board__input');
 
 boardBTN.addEventListener('click', (event) => {
     event.preventDefault()
     
-    let colums = input.value;
+    let column = BoardInput.value;
     let count;
 
-    if (colums >= 2 && colums <= 6 && colums % 2 == 0) {
-        count = colums * colums;
+    if (column >= 2 && column <= 6 && column % 2 == 0) {
+        count = column * column;
     } else {
         alert("Нужно написать четное число в указанном диапазоне.");
         return;
     }
 
-    createBoard(count, colums);
+    createBoard(column, count);
 })
 
-function createBoard(count, colums) {
+function createBoard(columns, count) {
     board.textContent = "";
+    let newBoard = document.querySelector('#gameTableTemplate').cloneNode(true).content;
+
+    const gameTable = newBoard.querySelector('.table');
+    const restartBTN = newBoard.querySelector('.table__button');
+
+    gameTable.style = `
+        grid-template-columns: repeat(${columns}, 1fr);
+        grid-template-rows: repeat(${columns}, 1fr);
+    `;
+
+    restartBTN.addEventListener("click", location.reload())
+    
+    for (let i = 0; i < count; i++) {
+        gameTable.append(createCard());
+    }
+
+    board.append(gameTable);
+    board.append(restartBTN);
+
+}
+
+function createCard() {
+
+    let newCard = document.querySelector('#cardTemplate').cloneNode(true).content;
+
+    const card = newCard.querySelector('.card')
 }
